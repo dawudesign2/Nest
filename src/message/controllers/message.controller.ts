2bff92bf3aa8df547d44bb9cccf0b610e8d1a719
Dawudesign <contact@dawudesign.fr>
2022-10-12 21:58:34 +0200

    adding routing

--- src/message/controllers/message.controller.ts
+++ src/message/controllers/message.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('message')
-export class MessageController {}
+export class MessageController {
+  @Get()
+  listMessage(): string {
+    return 'Hello World!';
+  }
+
+  @Get(':id')
+  getMessage(@Param('id') id: string): void {
+    console.log(id);
+  }
+
+  @Post()
+  createMessage(@Body() message: string): void {
+    console.log(message);
+  }
+}
